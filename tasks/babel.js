0274d3a2725c278556ed7f7a38dd40f23f3ee796
Long Le <long_le@intuit.com>
2015-10-26 19:56:08 -0700

    Add babel-relay to config file

--- tasks/babel.js
+++ tasks/babel.js
@@ -2,26 +2,33 @@
 var path = require('path');
 var babel = require('babel-core');
 var fs = require("fs");
-var getBabelRelayPlugin = require('babel-relay-plugin');
 
 module.exports = function (grunt) {
-	var schemaData = grunt.file.readJSON('./schema.json');
-	var plugin = getBabelRelayPlugin(schemaData.data);
 	grunt.registerMultiTask('babel', 'Transpile ES6 to ES5', function () {
 		var options = this.options();
 
 		this.files.forEach(function (el) {
 			delete options.filename;
 			delete options.filenameRelative;
+			var res = null;
+			if (options.relay) {
+				var getPlugin = require(options.relay.plugin);
+				var schema = grunt.file.readJSON(options.relay.schema);
+				var plugin = getPlugin(schema.data);
+				options.plugins = [plugin];
+				delete options.relay;
+
+				var source = fs.readFileSync(el.src[0]);
+				res = babel.transform(source, options);
+			} else {
+				res = babel.transformFileSync(el.src[0], options);
+			}
 
 			options.sourceFileName = path.relative(path.dirname(el.dest), el.src[0]);
 			if (process.platform === 'win32') {
 				options.sourceFileName = options.sourceFileName.replace(/\\/g, '/');
 			}
 			options.sourceMapTarget = path.basename(el.dest);
-			options.plugins = [plugin];
-			var source = fs.readFileSync(el.src[0]);
-			var res = babel.transform(source, options);
 
 			var sourceMappingURL = '';
 			if (res.map) {
